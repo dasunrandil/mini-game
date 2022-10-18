bf6e8544d115d56e1c69e6812d06b9fb1dd278e3
dasunrandil <dasunrandil@gmail.com>
2022-10-18 16:56:13 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,91 @@
+const player = document.getElementById('player');
+const ground = document.getElementById('ground');
+
+let dx = 0;
+let dy = 1;
+let acceleration = 0.3;
+let index = 0;
+
+const draw = ()=>{
+    if(dy != 0){
+        player.style.backgroundImage = `url('../img/templerun/Jump__00${index++}.png')`;
+    }else if (dx != 0){
+        player.style.backgroundImage = `url('../img/templerun/Run__00${index++}.png')`;
+    }else{
+        player.style.backgroundImage = `url('../img/templerun/Idle__00${index++}.png')`;
+    }   
+    if (index > 9) index = 0;
+
+    requestAnimationFrame(draw);
+}
+
+const animate = ()=>{
+
+    if ((player.offsetLeft + player.offsetWidth) > innerWidth){
+        dx =0;
+        player.style.left =`${innerWidth - player.offsetWidth}px`;
+    }else if(player.offsetLeft < 0){
+        dx =0;
+        player.style.left = '0';
+    }
+
+    dy += acceleration;
+    
+    if((player.offsetTop + player.offsetHeight) > ground.offsetTop){
+        dy =0;
+        player.style.top = `${ground.offsetTop - player.offsetHeight}px`;
+        acceleration = 0;
+    }
+
+    player.style.left = `${player.offsetLeft + dx}px`
+    player.style.top = `${player.offsetTop + dy}px`
+
+    requestAnimationFrame(animate);
+}
+
+addEventListener('keydown', ({key})=>{
+    if (key === 'ArrowRight'){
+        // index = 0;
+        player.classList.remove('turn');
+        dx = 10; 
+    }else if (key === 'ArrowLeft'){
+        // index = 0;
+        player.classList.add('turn');
+        dx = -10;
+    }
+});
+
+addEventListener('keypress', ({key})=>{
+    if (key === ' '){
+        dy = -10;
+        acceleration = 0.3;
+    }
+});
+
+addEventListener('keyup', ({key})=>{
+    if (key === 'ArrowRight' || key === 'ArrowLeft'){
+        dx = 0;
+    }
+});
+
+requestAnimationFrame(draw);
+requestAnimationFrame(animate);
+
+// let j = 0;
+// let t1 = 0;
+// const interval = 1; 
+
+// function repaint(timestamp){
+//     if (!t1) t1 = timestamp;
+//     const diff = timestamp -t1;
+//     if (diff >= (interval * 1000)){
+//         t1 = timestamp;
+//         console.log('painted', j++);
+//     }
+//     requestAnimationFrame(repaint);
+//     // console.log('timestamp', timestamp);
+// }
+
+// requestAnimationFrame(repaint);
+
+
